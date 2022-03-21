1aa419fd07726b1e2a4904b303d8d45b1e540a49
HyeYeonCha <gpdus335@naver.com>
2022-03-21 17:55:28 +0900

    initialize commit src

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    void main() {
+        // Given
+        String[] args = {"3", "1", "2"};
+
+        // When & Then
+        Main.main(args);
+    }
+}
